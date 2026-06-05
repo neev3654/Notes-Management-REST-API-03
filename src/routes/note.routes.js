cf316c6324bd7676b44979ac14991e74d28cebe1
neev3654 <neev.patel.cg@gmail.com>
2026-06-05 17:45:49 +0530

    feat: add search all endpoint (GET /api/notes/search/all)

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createNote, createBulkNotes, getAllNotes, getNoteById, replaceNote, updateNote, deleteNote, deleteBulkNotes, searchByTitle, searchByContent } = require("../controllers/note.controller");
+const { createNote, createBulkNotes, getAllNotes, getNoteById, replaceNote, updateNote, deleteNote, deleteBulkNotes, searchByTitle, searchByContent, searchAll } = require("../controllers/note.controller");
 
 // CRUD bulk
 router.post("/bulk", createBulkNotes);
@@ -8,6 +8,7 @@ router.delete("/bulk", deleteBulkNotes);
 
 // Search routes
 router.get("/search/content", searchByContent);
+router.get("/search/all", searchAll);
 router.get("/search", searchByTitle);
 
 // CRUD single-item routes
